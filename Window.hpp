e856321c4151e7c84e2362f35a7e0eeda1a7dbc7
SimilaTeam <similateam@gmail.com>
2025-04-01 22:46:36 +0300

    Clarified comments

--- Window.hpp
+++ Window.hpp
@@ -20,21 +20,33 @@
 namespace SIM {
 
     namespace Window {
-
+        
         /// VALUE_RESIZE
         /*
             >! @brief Stabilizes the window size.
             -
-            >! @return By stabilizing the window size, it takes in consideration the original size and resizes the window to a multiple of that.
+            >! @details By stabilizing the window size, it takes in consideration the original size and resizes the window to a multiple of that.
+        */
+
+        void Value_Resize (sf::RenderWindow& Window, const sf::Vector2u& Original_Size, const sf::Vector2u& Desired_Size) {
+            Window.setSize (sf::Vector2u( ((float)Desired_Size.y/(float)Original_Size.y) * Original_Size.x, ((float)Desired_Size.y/(float)Original_Size.y) * Original_Size.y) );
+        }
+
+        /// VALUE_RESIZE (MONITOR DEPENDENT)
+        /*
+            >! @brief Stabilizes the window size based on the monitor resolution.
+            -
+            >! @details By stabilizing the window size, it takes in consideration the resolution of the user's screen and resizes the window to a multiple of that.
         */
 
         void Value_Resize (sf::RenderWindow& Window, const sf::Vector2u& Original_Size) {
             Window.setSize (sf::Vector2u( ((float)sf::VideoMode().getDesktopMode().height/(float)Original_Size.y) * Original_Size.x, ((float)sf::VideoMode().getDesktopMode().height/(float)Original_Size.y) * Original_Size.y) );
         }
-
-        void Value_Resize (sf::RenderWindow& Window, const sf::Vector2u& Original_Size, const sf::Vector2u& Desired_Size) {
-            Window.setSize (sf::Vector2u( ((float)Desired_Size.y/(float)Original_Size.y) * Original_Size.x, ((float)Desired_Size.y/(float)Original_Size.y) * Original_Size.y) );
-        }
+        
+        /// VALUE_RESIZE (MONITOR DEPENDENT)
+        /*
+            >! @brief Centers the window based on the screen size.
+        */
 
         void Center_Window (sf::RenderWindow& Window) {
             Window.setPosition (sf::Vector2i (sf::VideoMode().getDesktopMode().width/2 - Window.getSize().x/2, sf::VideoMode().getDesktopMode().height/2 - Window.getSize().y/2) );
