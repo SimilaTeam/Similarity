93b702373f6256c34604d896e494475373356c7a
SimilaTeam <similateam@gmail.com>
2025-04-02 21:11:19 +0300

    Repaired Hitbox logic + added comments

--- UI/SpriteUI.hpp
+++ UI/SpriteUI.hpp
@@ -40,26 +40,50 @@ namespace SIM {
 
             sf::FloatRect Hitbox;
             bool Hitbox_Dependency = true;
+            
+            /// SETPOSITION
+            /*
+                >! @brief Changes the position of the UI.
+                -
+                >! @param position - the position.
+            */
 
             void setPosition (const sf::Vector2f& Position) {
 
-                if (Object.isGeoCentered) {
-
-                    if (Hitbox_Dependency) {
-                        Hitbox.width = Object.Sprite.getLocalBounds().width;
-                        Hitbox.height = Object.Sprite.getLocalBounds().height;
-                    }
-
-                    Hitbox.left = Object.Sprite.getPosition().x - (Hitbox.width/2);
-                    Hitbox.top = Object.Sprite.getPosition().y - (Hitbox.height/2);
-
+                if (Hitbox_Dependency && Hitbox != Object.Sprite.getGlobalBounds()) {
+                    Hitbox = Object.Sprite.getGlobalBounds();
                 }
 
                 Object.Sprite.setPosition(Position);
             }
 
+            /// MOVE
+            /*
+                >! @brief Moves the UI.
+                -
+                >! @details This function moves the UI based on an offset.
+                -
+                >! @param Offset - the position offset.
+            */
+            
             void move (const sf::Vector2f& Offset) {
                 setPosition (sf::Vector2f (Object.Sprite.getPosition().x + SIM::Time::DeltaTime.asSeconds()*Offset.x, Object.Sprite.getPosition().y + SIM::Time::DeltaTime.asSeconds()*Offset.y) );
+            }
+            
+            /// DRAW
+            /*
+                >! @brief Draws the UI to a window.
+                -
+                >! @param Window - the window the UI will be drawn to.
+            */
+
+            void draw (sf::RenderWindow& Window) {
+
+                if (Hitbox_Dependency && Hitbox != Object.Sprite.getGlobalBounds()) {
+                    Hitbox = Object.Sprite.getGlobalBounds();
+                }
+
+                Window.draw (Object.Sprite);
             }
 
         };
